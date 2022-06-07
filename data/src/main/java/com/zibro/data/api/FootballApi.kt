547940bb7c617e4e9fa511903c97b5e7e9cad0bb
zibro <jgh6272@naver.com>
2022-06-07 23:58:06 +0900

    🚀 rocket : FootballApi Interface 필요한 Api Request 정의

--- data/src/main/java/com/zibro/data/api/FootballApi.kt
+++ data/src/main/java/com/zibro/data/api/FootballApi.kt
@@ -1,12 +1,80 @@
 package com.zibro.data.api
 
-import com.zibro.data.BuildConfig
 import com.zibro.domain.entity.StatusInfo
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Query
 
+/**
+ * api-sprots Request 규칙
+ * 1. GET 요청에서만 동작하도록 구성
+ * 2. 아래 나열된 헤더만 허용
+ *      a. x-rapidapi-host
+ *      b. x-rapidapi-key
+ *      c. x-apisports-key
+ * 3. 대회 로고 url https://media.api-sports.io/football/leagues/{league_id}.png
+ *
+ * */
 interface FootballApi {
     //test api Request
     @GET("/")
     suspend fun status():StatusInfo
+
+    //국가 검색
+    @GET("/countries")
+    suspend fun countries(
+        //국가 명
+        @Query("name")
+        //국가 알파벳 코드 ex) FR,GB,IT ...
+        countryName : String
+    )
+
+    //리그 및 컵 목록 검색
+    @GET("/leagues")
+    suspend fun leagueInfo(
+        @Query("name")
+        leagueName : String,
+    )
+
+    //팀정보 (팀ID, 로고..등)
+    @GET("/teams")
+    suspend fun teamList(
+        @Query("id")
+        teamId : Int?,
+        @Query("name")
+        teamName : String?,
+        @Query("league")
+        leagueId : Int?,
+        @Query("season")
+        season : Int?,
+        @Query("country")
+        country : String?,
+        @Query("search")
+        keyword : String?
+    )
+
+    //팀 통계
+    @GET("/teams/statistics")
+    suspend fun teamStatistics(
+        @Query("league")
+        league : Int,
+        @Query("season")
+        season : Int,
+        @Query("team")
+        teamId : Int,
+        @Query("date")
+        dateLimit : String?
+    )
+
+    //리그 또는 팀의 테이블
+    @GET("standings")
+    suspend fun leagueOrTeamTable(
+        @Query("league")
+        leagueId: Int?,
+        @Query("season")
+        season: Int,
+        @Query("team")
+        teamId: Int?
+    )
+
+
 }
--- presentation/src/main/java/com/zibro/presentation/util/Constants.kt
+++ presentation/src/main/java/com/zibro/presentation/util/Constants.kt
@@ -4,5 +4,5 @@ package com.zibro.presentation.util
  * BaseURL 상수 Object 클래스
  * */
 object Constants {
-    const val FOOTBALL_BASE_URL = "https://v3.football.api-sports.io/"
+    const val FOOTBALL_BASE_URL = "https://v3.football.api-sports.io"
 }
