474c485d1be78842f1b9db43b5e69e32bfda2137
zibro <jgh6272@naver.com>
2022-07-25 23:07:43 +0900

    22/07/25 🏗️ : commit

--- presentation/src/main/java/com/zibro/presentation/ui/news/NewsFragment.kt
+++ presentation/src/main/java/com/zibro/presentation/ui/news/NewsFragment.kt
@@ -1,9 +1,14 @@
 package com.zibro.presentation.ui.news
 
+import androidx.fragment.app.viewModels
 import com.zibro.presentation.R
 import com.zibro.presentation.base.BaseFragment
 import com.zibro.presentation.databinding.FragmentNewsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-class NewsFragment : BaseFragment<FragmentNewsBinding>(R.layout.fragment_news) {
+@AndroidEntryPoint
+class NewsFragment : BaseFragment<FragmentNewsBinding,NewsViewModel>(R.layout.fragment_news) {
+    override val viewModel by viewModels<NewsViewModel>()
+
+    override fun observeData() = Unit
 }
